457ce923e1055eaab167923a72b0f0e277fc80a3
Madison Anderson <mranderson@miners.utep.edu>
2021-11-09 14:37:23 -0700

    blinking project

--- project/blink/3-led-blink/main.c
+++ project/blink/3-led-blink/main.c
@@ -24,7 +24,8 @@ __interrupt_vec(WDT_VECTOR) WDT()	/* 250 interrupts/sec */
   secondCount ++;
   if (secondCount >= 125) { 	/* once each sec... */
     secondCount = 0;		/* reset count */
-    P1OUT ^= LED_GREEN;		/* toggle green LED */
+    P1OUT ^= LED_GREEN;   /* toggle green LED */
+    P1OUT &= ~LED_RED;
   }
 } 
 
